f747008673ee7e013e030c1720663ec1af6f57ee
punith3527 <punithreddy7253@gmail.com>
2022-11-04 20:12:48 +0530

    added density output table

--- src/main/java/com/example/roadtestings1/Density/DensityOutputActivity.java
+++ src/main/java/com/example/roadtestings1/Density/DensityOutputActivity.java
@@ -1,6 +1,17 @@
 package com.example.roadtestings1.Density;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.roadtestings1.R;
+
 public class DensityOutputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.density_output);
+    }
 }
