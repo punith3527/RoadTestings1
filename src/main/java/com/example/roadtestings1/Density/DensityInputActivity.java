43e60aa6d357f4ddb1a00eae80d86e424f95dc9f
punith3527 <punithreddy7253@gmail.com>
2022-11-05 18:22:09 +0530

    final changes

--- src/main/java/com/example/roadtestings1/Density/DensityInputActivity.java
+++ src/main/java/com/example/roadtestings1/Density/DensityInputActivity.java
@@ -26,7 +26,7 @@ public class DensityInputActivity extends AppCompatActivity {
     private static final DecimalFormat df1 = new DecimalFormat("0.000");
 
     AppCompatButton toTable;
-    EditText x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13;
+    EditText x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -47,9 +47,8 @@ public class DensityInputActivity extends AppCompatActivity {
         x8 = findViewById(R.id.volume);
         x9 = findViewById(R.id.w);
         x10 = findViewById(R.id.bulk);
-        x11 = findViewById(R.id.fieldDry);
-        x12 = findViewById(R.id.labDensity);
-        x13 = findViewById(R.id.relative);
+        x11 = findViewById(R.id.labDensity);
+        x12 = findViewById(R.id.relative);
 
         x2.addTextChangedListener(new TextWatcher() {
             @Override
@@ -149,23 +148,6 @@ public class DensityInputActivity extends AppCompatActivity {
             }
         });
 
-        x12.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                OnTextChanged();
-            }
-        });
-
         toTable = findViewById(R.id.toTable);
 
         toTable.setOnClickListener(v -> {
@@ -176,8 +158,7 @@ public class DensityInputActivity extends AppCompatActivity {
                     TextUtils.isEmpty(x5.getText().toString()) ||
                     TextUtils.isEmpty(x7.getText().toString()) ||
                     TextUtils.isEmpty(x9.getText().toString()) ||
-                    TextUtils.isEmpty(x11.getText().toString()) ||
-                    TextUtils.isEmpty(x12.getText().toString())
+                    TextUtils.isEmpty(x11.getText().toString())
             ) {
                 Toast.makeText(this, "Input Field can't be empty", Toast.LENGTH_SHORT).show();
             } else {
@@ -195,7 +176,6 @@ public class DensityInputActivity extends AppCompatActivity {
                 intent.putExtra("j2", x10.getText().toString());
                 intent.putExtra("k2", x11.getText().toString());
                 intent.putExtra("l2", x12.getText().toString());
-                intent.putExtra("m2", x13.getText().toString());
                 startActivity(intent);
             }
         });
@@ -228,11 +208,11 @@ public class DensityInputActivity extends AppCompatActivity {
             x10.setText(df1.format(c));
         }
 
-        if (!TextUtils.isEmpty(x11.getText().toString()) && !TextUtils.isEmpty(x12.getText().toString())) {
-            double a = Double.parseDouble(x11.getText().toString());
-            double b = Double.parseDouble(x12.getText().toString());
+        if (!TextUtils.isEmpty(x10.getText().toString()) && !TextUtils.isEmpty(x11.getText().toString())) {
+            double a = Double.parseDouble(x10.getText().toString());
+            double b = Double.parseDouble(x11.getText().toString());
             double c =a * 100.00 / b;
-            x13.setText(df.format(c));
+            x12.setText(df.format(c));
         }
     }
 
